f4e283eccd71996bc0d7f3ab0a39ef5ee24a5bc0
Sophia <sophia.kuznetsova.7@gmail.com>
2021-06-23 07:36:27 +1000

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,11 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig= new Twig_Environment($loader);
 
 $template = $twig->loadTemplate('index.twig');
-$title  = "ok";
+$title  = "Car";
     $car=array(
     array('title'=> 'Toyota Camry', 'engine volume'=>'3.5'),
     array('title'=> 'Nissan Juke', 'engine volume'=>'1.5'),
     array('title'=> 'Lexus GX', 'engine volume'=>'4.6'),
     array('title'=> 'Lexus LX', 'engine volume'=>'5.7'),
-
-);
+    );
  echo $template->render('data.twig',array('car'=>$car));
